fd532f91d097610ae510a5809caefc2de3de3db4
Agung Sasongko <agoenxz21@gmail.com>
2024-01-30 17:11:03 +0700

    update

--- app/http/controllers/webhook_controller.go
+++ app/http/controllers/webhook_controller.go
@@ -5,6 +5,8 @@ import (
 	"github.com/goravel/framework/contracts/http"
 	"github.com/goravel/framework/facades"
 	"github.com/goravel/framework/support/json"
+	"os/exec"
+	"strings"
 )
 
 type WebhookController struct {
@@ -53,4 +55,94 @@ func (r *WebhookController) Index(ctx http.Context) http.Response {
 
 	}
 	return ctx.Response().String(200, tmp)
+}
+
+func ListenWebHook(ctx http.Context) http.Response {
+	var payload map[string]interface{}
+	err := ctx.Request().Bind(&payload)
+	if err != nil {
+		return ctx.Response().Json(401, http.Json{
+			"message": err.Error(),
+		})
+	}
+
+	fileconfig, errconfig := facades.Storage().Get("config.js")
+	if errconfig != nil {
+		return ctx.Response().Json(401, http.Json{
+			"message": "Tidak ada configurasi di tentukan",
+		})
+	}
+
+	var js map[string]interface{}
+	errUm := json.Unmarshal([]byte(fileconfig), &js)
+	if errUm != nil {
+		return ctx.Response().Json(401, http.Json{
+			"message": "Gagal membuka file config",
+		})
+	}
+
+	elRepository, ok := js["repositories"].([]interface{})
+	if !ok {
+		return ctx.Response().Json(401, http.Json{
+			"message": "tidak ada repository diatur pada config",
+		})
+	}
+
+	elRef, ok := js["ref"].(string)
+
+	namePush := getNamePush(ctx)
+
+	for _, repo := range elRepository {
+		repoMap, ok := repo.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		nameRef, ok := repoMap["ref"].(string)
+		nameRepo, ok := repoMap["name"].(string)
+
+		if nameRepo == *namePush {
+			if elRef == nameRef {
+				direktori, _ := repoMap["directory"].(string)
+				cmds := repoMap["command"].([]interface{})
+				eksekusi(direktori, cmds)
+			}
+		}
+	}
+
+	return ctx.Response().Json(200, http.Json{
+		"message": "OK",
+	})
+}
+
+func eksekusi(direktori string, cmds []interface{}) {
+	for _, perintah := range cmds {
+		pp := strings.Split(perintah.(string), " ")
+		cmd := exec.Command(pp[0], pp[1:]...)
+		cmd.Dir = direktori
+		_, er := cmd.CombinedOutput()
+		if er != nil {
+			continue
+		}
+	}
+}
+
+func getNamePush(ctx http.Context) *string {
+	var js map[string]interface{}
+
+	err := ctx.Request().Bind(&js)
+	if err != nil {
+		return nil
+	}
+
+	repo, ok := js["repository"].(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	name, ok := repo["name"].(string)
+	if !ok {
+		return nil
+	}
+	return &name
 }
---
+++ storage/app/config.js
@@ -0,0 +1,20 @@
+{
+    "repositories":[
+        {
+            "name": "mobile-apps-m-staff",
+            "ref": "refs/heads/api-master",
+            "directory": "/home/bti/api/m-staff",
+            "command": [
+                "run.sh"
+            ]
+        },
+        {
+            "name": "mobile-apps-m-staff",
+            "ref": "refs/heads/api-master",
+            "directory": "/home/bti/api/m-staff",
+            "command": [
+                "run.sh"
+            ]
+        },
+    ]
+}
